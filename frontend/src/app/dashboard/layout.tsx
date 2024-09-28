afb59b75398a50b12f92250d9cf9a691f06bbe3a
whosysAndre <yellowcata2121@gmail.com>
2024-09-28 13:04:26 -0500

    Auth finish

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -6,7 +6,7 @@ export default function DashboardLayout({
 }) {
   return (
     <>
-      
+      {children}
     </>
   );
 }
--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@
 export default function HomePage() {
   return (
     <div>
-      <h1>Hello Page</h1>
+      <h1>Hello Pagedwdad</h1>
     </div>
   );
 }
