f9b51318c79f163d14c1daabc8274eed27e0bcac
Lord <toledosandro59@gmail.com>
2025-06-07 06:29:42 -0500

    añadiendo data-source

---
+++ backend/data-source.ts
@@ -0,0 +1,17 @@
+import { DataSource } from 'typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+// Cargar variables de entorno
+ConfigModule.forRoot();
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: ['src/**/*.entity.ts'],
+  migrations: ['src/migrations/*.ts'],
+  synchronize: false,
+});
