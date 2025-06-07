df67dc464b2d0c3de2dabf0728c00a6d69b8568e
Lord <toledosandro59@gmail.com>
2025-06-07 04:15:15 -0500

    Mejora en parametros de [id] en client

--- frontend/src/app/dashboard/clientes/[id]/page.tsx
+++ frontend/src/app/dashboard/clientes/[id]/page.tsx
@@ -4,8 +4,7 @@ import { Title } from "@ui/Title";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-export default async function ClientPage({ params }: { params: { id: string } }) {
-
+export default async function ClientPage({ params }: { params: Promise<{ id: string }> }) {
   const cookieStore = await cookies();
   const myCookie = cookieStore.get('authToken');
 
@@ -13,19 +12,20 @@ export default async function ClientPage({ params }: { params: { id: string } })
     redirect('/auth/login');
   }
 
-  const client = await getClient(myCookie.value, params.id);
-
+  // Await params para obtener el id
+  const { id } = await params;
+  const client = await getClient(myCookie.value, id);
 
   return (
     <div className="w-full shadow rounded-lg am:p-4 sm:p-4 md:p-6 xl:p-8 flex flex-col gap-4 h-[calc(100vh-130px)]">
       <div className="flex items-center justify-between">
-              <div className="flex flex-col gap-2">
-                <Title
-                  title="Detalle de cliente"
-                  subTitle="Vista detallada de un cliente"
-                />
-              </div>
-            </div>
+        <div className="flex flex-col gap-2">
+          <Title
+            title="Detalle de cliente"
+            subTitle="Vista detallada de un cliente"
+          />
+        </div>
+      </div>
       <DetailsClient
         client={client}
       />
