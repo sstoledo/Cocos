7f81198f4059a35f9e6a717ac05f4c57b21d8298
Lord <toledosandro59@gmail.com>
2025-06-07 06:53:08 -0500

    mejora

---
+++ backend/data-source.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AppDataSource = void 0;
+const typeorm_1 = require("typeorm");
+const config_1 = require("@nestjs/config");
+config_1.ConfigModule.forRoot();
+exports.AppDataSource = new typeorm_1.DataSource({
+    type: 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT) || 5432,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: ['src/**/*.entity.ts'],
+    migrations: ['src/migrations/*.ts'],
+    synchronize: false,
+});
+//# sourceMappingURL=data-source.js.map
